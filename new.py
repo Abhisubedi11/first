6ceead61c7fe415d4248aa06fa00aae8f267903c
Abhisubedi11 <subediabhi14@gmail.com>
2025-05-23 08:27:05 +0545

    first

--- new.py
+++ new.py
@@ -1,4 +1,18 @@
-print ("My name is abhi/n i am from itahari")
-a=55555
-b=55555
-print(a+b)
+print("basic calculator")
+number1 = float(input("enter first number:"))
+number2 = float(input("enter second number:"))
+print("press 1 for add \n2 for subtraction \n3 for multiplication \n 4 for division")
+
+choice = int(input("enter youre choice from 1 to 4:"))
+
+if choice == 1:
+print ("the sum is:", number1 + number2)
+elif choice == 2:
+print("the subtraction is:", number1 - number2)
+elif choice == 3:
+print("the multiplication is:", number1 * number2 )
+elif choice == 4:
+print("the division is:", number1 / number2)
+else :
+print("INvalid input")
+
